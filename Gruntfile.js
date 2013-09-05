d12fc6569d18b112c8ade3e46f2fd446e3b6cb55
Christian Smith <smith@anvil.io>
2013-09-05 13:14:00 -0700

    extracted Model/Backend from OAuth2Server

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-cov');
+
+  grunt.initConfig({
+    watch: {
+      test: {
+        files: [
+          'lib/**/*.js',
+          'test/**/*.js'
+        ],
+        tasks: ['mochacov']
+      }
+    },
+    mochacov: {
+      options: {
+        reporter: 'spec'
+      },
+      all: [
+        'test/**/*.js'
+      ]
+    }
+  });
+
+  grunt.registerTask('test', ['mochacov', 'watch:test']);
+  
+};
