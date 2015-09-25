dff322a459a80bd32533b46ae52aff7a0049b95f
Vartan Simonian <vsimonianpress@gmail.com>
2015-09-24 21:33:15 -0700

    Use mocha directly for test script instead of grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,4 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-watch')
-  grunt.loadNpmTasks('grunt-mocha-cov')
   grunt.loadNpmTasks('grunt-release')
-
-  grunt.initConfig({
-    watch: {
-      test: {
-        files: [
-          'lib/**/*.js',
-          // 'test/**/*.js',
-          'test/**/*.coffee'
-        ],
-        tasks: ['mochacov']
-      }
-    },
-    mochacov: {
-      options: {
-        reporter: 'spec',
-        compilers: ['coffee:coffee-script/register']
-      },
-      all: [
-        'test/**/*.coffee'
-      ]
-    }
-  })
-
-  grunt.registerTask('test', ['mochacov', 'watch:test'])
-
+  grunt.initConfig({})
 }
